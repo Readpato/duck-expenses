38110e0f0869dd72f9a284430a880a55bd68ee4a
Patrick Raedler <code@readpato.dev>
2023-10-28 19:43:16 +0200

    feat(TheHeader): create header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const isCollapsed = useMenuState()
+</script>
+
+<template>
+  <div class="w-full h-20 text-black p-4">
+    <button class="flex justify-center items-center" @click="isCollapsed = !isCollapsed">
+      <Icon name="carbon:up-to-top" class="text-xl -rotate-90" :class="{ 'rotate-90': isCollapsed }" />
+    </button>
+  </div>
+</template>
